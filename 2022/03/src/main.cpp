a57cbe98ad79f26a7f4db4b2025d12a2c49b14b3
Guillaume MARTY <guillaume.f.marty@gmail.com>
2022-12-04 16:29:35 +0100

    Add days 3 and 4 solvers.

---
+++ 2022/03/src/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include "AdventOfCode_utils.hpp"
+
+using namespace std;
+
+int getPriority(char c);
+
+
+int main (int argc, char **argv) {
+    string str_input = readTxtFile2String("input_03.txt");
+    vector<string> strVect_split = str_split(str_input, '\n');
+
+    //Part 1
+    //#############################################################################
+    int int_sumP = 0;
+    string::iterator it_dupplicatedPrio;
+    for (auto it = strVect_split.begin() ; it != strVect_split.end(); ++it) {
+        it_dupplicatedPrio = find_first_of( (*it).begin(), (*it).begin() + ((*it).size())/2 - 1, (*it).begin() + ((*it).size())/2, (*it).end());
+        int_sumP += getPriority(*it_dupplicatedPrio);
+    }
+
+    cout << "Day 3 - Part1  anwswer : " << int_sumP << endl;
+    //#############################################################################
+
+    //Part 2
+    //#############################################################################
+    int_sumP = 0;
+
+    vector<string>::iterator it_shortestStr;
+    for (auto it_grp = strVect_split.begin() ; it_grp != strVect_split.end(); it_grp=it_grp+3) {
+
+        //List items(char) contains in each bag of current group, without duplication
+        set<char> set_bag1_content, set_bag2_content, set_bag3_content;
+        set_bag1_content.insert((*it_grp).begin(), (*it_grp).end());
+        set_bag2_content.insert((*(it_grp+1)).begin(), (*(it_grp+1)).end());
+        set_bag3_content.insert((*(it_grp+2)).begin(), (*(it_grp+2)).end());
+
+
+        for (auto it_char=set_bag1_content.begin() ; it_char != set_bag1_content.end() ; ++it_char) {
+            if (auto it_find = set_bag2_content.find(*it_char); it_find != set_bag2_content.end()){
+                if (auto it_find = set_bag3_content.find(*it_char); it_find != set_bag3_content.end()) {
+                    int_sumP += getPriority(*it_char);
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << "Day 3 - Part2  anwswer : " << int_sumP << endl;
+    //#############################################################################
+
+    return 0;
+}
+
+int getPriority(char c) {
+
+    //Upper case letters are before lower case ones in ASCII table, but there are not consecutive
+
+    if ( ((int) c < (int) 'A') || ((int) c > (int) 'z') || ( ((int) c > (int) 'Z') && ((int) c < (int) 'a')) ) {
+        cout << "getPriority : specified char '" << c << "' is outside expected range [a-zA-A]" << endl;
+        exit;
+    }
+
+    if ((int) c < (int) 'a') {
+        return 27 + (int) c - (int) 'A' ;
+    }
+    else {
+        return 1 + (int) c - (int) 'a' ;
+    }
+}
---
+++ 2022/04/src/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+struct ElvesPair
+{
+	int elf1_b, elf1_e, elf2_b, elf2_e;
+};
+istream &operator >> (istream &is, ElvesPair &elvesPair);
+ostream &operator << (ostream &os, ElvesPair &elvesPair);
+
+
+
+int main (int argc, char **argv) {
+
+    ifstream ifs_input ("input_04.txt");
+
+    ElvesPair elvesPair;
+    int int_sumFullOverlap = 0, int_sumPartialOverlap = 0;
+
+    while (ifs_input >> elvesPair) {
+        if (  ((elvesPair.elf1_b >= elvesPair.elf2_b) && (elvesPair.elf1_e <= elvesPair.elf2_e)) || ((elvesPair.elf2_b >= elvesPair.elf1_b) && (elvesPair.elf2_e <= elvesPair.elf1_e)) ){
+            int_sumFullOverlap++;
+            cout << "[DEBUG] : FULL overlap    : " << elvesPair << endl;
+        }
+        else if (!( (elvesPair.elf1_e < elvesPair.elf2_b) || (elvesPair.elf1_b > elvesPair.elf2_e) )){
+            int_sumPartialOverlap++;
+            cout << "[DEBUG] : PARTIAL overlap : " << elvesPair << endl;
+        }
+        else
+            cout << "[DEBUG] : NO overlap      : " << elvesPair << endl;
+    }
+
+    cout << "Day 4 - Part1  anwswer : " << int_sumFullOverlap << endl;
+    cout << "Day 4 - Part2  anwswer : " << int_sumFullOverlap + int_sumPartialOverlap << endl;
+
+    return 0;
+}
+
+
+istream &operator >> (istream &is, ElvesPair &elvesPair)
+{
+	is >> elvesPair.elf1_b;
+	is.ignore(1,'-');
+    is >> elvesPair.elf1_e;
+	is.ignore(1,',');
+    is >> elvesPair.elf2_b;
+    is.ignore(1,'-');
+    is >> elvesPair.elf2_e;
+	return is;
+}
+
+ostream &operator << (ostream &os, ElvesPair &elvesPair)
+{
+	os << "[" << elvesPair.elf1_b << "-" << elvesPair.elf1_e << "] -- [" << elvesPair.elf2_b << "-" << elvesPair.elf2_e << "]";
+	return os;
+}
