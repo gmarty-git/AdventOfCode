bcac9b219ce48f1212f8e797e8683c4d553d9945
Guillaume MARTY <guillaume.f.marty@gmail.com>
2023-01-05 00:22:44 +0100

    Day 16 part1 : run but generate toooo much pressure. To be fixed !

--- 2022/16/main.cpp
+++ 2022/16/main.cpp
@@ -6,8 +6,13 @@
 #include <vector>
 #include <map>
 #include <utility>  //pair
+#include <algorithm> //next_permutation (not used), all_of
+//#include <numeric> //iota
 #include <regex>
+#include <boost/multi_array.hpp>
 #include <boost/graph/adjacency_list.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/johnson_all_pairs_shortest.hpp>
 #include <boost/graph/graphviz.hpp>
 
 //using std::literals::string_literals::operator""s;
@@ -74,10 +79,11 @@ void simplifyGraph(Graph& g){
     Graph::vertex_iterator v, vend;
     Graph::out_edge_iterator e, eend, e2, e2end; 
     bool graphUpdate = true;
-    unsigned newEdgeWeight;
+    unsigned newEdgeWeight, vertexIdx;
     
     while (graphUpdate){
         graphUpdate = false;
+        vertexIdx = 0;
         for (tie(v, vend) = boost::vertices(g); v != vend; ++v) {
             if ( (g[*v].flow == 0) && (g[*v].name != "AA") ){
                 for (tie(e, eend) = boost::out_edges(*v, g) ; e != eend; ++e) {
@@ -93,8 +99,152 @@ void simplifyGraph(Graph& g){
                 graphUpdate = true;
                 break;        
             }
+            
+            g[*v].id = vertexIdx;
+            vertexIdx++;    
         }
     }
+}
+
+unsigned int getGraphVerticesCount(Graph& g){
+    Graph::vertex_iterator v, vend;
+    tie(v, vend) = boost::vertices(g);
+    return std::distance(v, vend);
+}
+
+typedef boost::multi_array<unsigned int, 2> array_type;
+array_type getShortestDistanceMatrix(Graph& g){
+    unsigned int verticesCount = getGraphVerticesCount(g);  
+    array_type D(boost::extents[verticesCount][verticesCount]);
+    boost::johnson_all_pairs_shortest_paths(g, D, boost::weight_map(boost::get(&EdgeProperties::weight, g)));
+    
+    return D;
+}
+
+
+unsigned long long playVerticesSequence(Graph& g, array_type& shortestDistancesMap,std::vector<unsigned int> verticesIdxSeq, unsigned int timeout){
+    unsigned long long  releasedPressure = 0ULL;
+    unsigned int totalFlow = 0, sourceVertexIdx, targetVertexIdx, moveDuration;
+    for (auto v = verticesIdxSeq.begin() + 1 ; v != verticesIdxSeq.end() ; ++v){
+                
+        //Move to next valve - some pressure is released during move
+        sourceVertexIdx =  *(v-1);
+        targetVertexIdx =  *v;
+        moveDuration = shortestDistancesMap[sourceVertexIdx][targetVertexIdx];
+        releasedPressure += (unsigned long long) totalFlow*moveDuration;
+        timeout -= moveDuration;
+        
+        //If it remains time open valve - some pressure is released during valve opening
+        if (timeout > 0){
+            timeout--;
+            releasedPressure += (unsigned long long)totalFlow;
+            totalFlow +=  g[*v].flow;
+        }
+        else { //If time exceed timeout, corrects relased pressure
+            releasedPressure -= (unsigned long long) (-timeout)*totalFlow;
+            break;
+        }
+    }
+    
+    if (timeout > 0)
+        releasedPressure += (unsigned long long) timeout*totalFlow;
+        
+    return releasedPressure;
+}
+
+/*unsigned int playVerticesSequence(Graph& g, array_type& shortestDistancesMap,std::vector<vertex_t> verticesSeq, unsigned int timeout){
+    unsigned int releasedPressure = 0, totalFlow = 0, sourceVertexIdx, targetVertexIdx, moveDuration;
+    for (auto v = verticesSeq.begin() + 1 ; v != verticesSeq.end() ; ++v){
+                
+        //Move to next valve - some pressure is released during move
+        sourceVertexIdx =  g[*(v-1)].id;
+        targetVertexIdx =  g[*v].id;
+        moveDuration = shortestDistancesMap[sourceVertexIdx][targetVertexIdx];
+        releasedPressure += totalFlow*moveDuration;
+        timeout -= moveDuration;
+        
+        //If it remains time open valve - some pressure is released during valve opening
+        if (timeout > 0){
+            timeout--;
+            releasedPressure += totalFlow;
+            totalFlow +=  g[*v].flow;
+        }
+        else { //If time exceed timeout, corrects relased pressure
+            releasedPressure += timeout*totalFlow;
+            break;
+        }
+    }
+    
+    if (timeout > 0)
+        releasedPressure += timeout*totalFlow;
+        
+    return releasedPressure;
+}*/
+
+void getPermutations(   Graph& g, 
+                        array_type& shortestDistancesMap, 
+                        unsigned int timeout, 
+                        std::vector<std::vector<unsigned int> >& allPermutations, 
+                        std::vector<unsigned int> currentPermutation = {},
+                        std::vector<bool> usedIdx = {},
+                        unsigned int usedTime = 0,
+                        int nextVertexIdx = -1){
+
+    unsigned int verticesCount = getGraphVerticesCount(g);
+
+    //Initialise permutation to 'AA'
+    if (currentPermutation.empty()){
+        unsigned int AA_idx;
+        Graph::vertex_iterator v, vend;
+        for ( std::tie(v, vend) = boost::vertices(g); v != vend; ++v) {
+            if (g[*v].name == "AA"){
+                AA_idx = g[*v].id;
+                break;
+            }
+        }
+
+        currentPermutation.reserve(verticesCount);
+        currentPermutation.push_back(AA_idx);
+        usedIdx.resize(verticesCount, false);
+        usedIdx[AA_idx] = true;
+    }
+
+    //If not root call, then add specify vertex to permutation and update usedTime
+    if (nextVertexIdx != -1){
+        usedIdx[nextVertexIdx] = true;
+        currentPermutation.push_back(nextVertexIdx);
+        usedTime += shortestDistancesMap[currentPermutation[currentPermutation.size()-2]][nextVertexIdx] + 1 ; //+1 time to open valve
+    }
+
+    //If timeout is reached or all valves have been open then save current permutation and break recursion
+    if ( (usedTime >= timeout) || (std::all_of(usedIdx.begin(), usedIdx.end(), [](bool v) { return v; })) ) {
+        allPermutations.push_back(currentPermutation);
+        return;
+    }
+    else{
+        nextVertexIdx = 0;
+        for (auto usedV = usedIdx.begin() ; usedV != usedIdx.end() ; ++usedV){
+            if (!*usedV)
+                getPermutations(g, shortestDistancesMap, timeout, allPermutations, currentPermutation, usedIdx, usedTime, nextVertexIdx);
+            
+            nextVertexIdx++;
+        }   
+    }    
+}
+
+unsigned long long getMaxReleasedPressure(Graph& g, array_type& shortestDistancesMap, unsigned int timeout){
+    std::vector<std::vector<unsigned int> > allPermutations;
+    getPermutations(g, shortestDistancesMap, timeout, allPermutations);
+
+    //Naive approach : simulate all paths, even obviously ineficient ones
+    unsigned long long max_releasedP = 0ULL, releasedP = 0ULL;
+    for (auto perm = allPermutations.begin() ; perm != allPermutations.end() ; ++perm){
+        releasedP = playVerticesSequence(g, shortestDistancesMap, *perm, timeout);
+        if (releasedP > max_releasedP)
+            max_releasedP = releasedP;
+    }
+    
+    return max_releasedP;
 }
 
 template <class Name, class Flow>
@@ -153,15 +303,16 @@ void writeGraphToDotFile(std::string filename, Graph& g, bool hideEdgeWeight = f
 
 int main (int argc, char **argv) { 
 
-    Graph g, g_simplified;
+    Graph g;
     readGraphReportFromFile("input_16.txt", g);
-     
-    g_simplified = g;
-    simplifyGraph(g_simplified);
-    
     writeGraphToDotFile("tunnelsGraph.dot", g, true);
-    writeGraphToDotFile("valvesGraph.dot", g_simplified);
-    cout << "Day 16 - Part1  anwswer : " << "" << endl;
+    
+    simplifyGraph(g);
+    writeGraphToDotFile("valvesGraph.dot", g);
+    array_type shortestDistancesMap = getShortestDistanceMatrix(g);
+ 
+    
+    cout << "Day 16 - Part1  anwswer : " << getMaxReleasedPressure(g, shortestDistancesMap, 30)  << endl;
     cout << "Day 16 - Part2  anwswer : " << "" << endl;
 
     exit(0);
